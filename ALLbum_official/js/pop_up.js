ad91402f757d0087db286a3ba9c0a9a71e2eecc8
takecy <yamacy.takecy@gmail.com>
2012-09-21 00:42:37 +0900

    recent

--- ALLbum_official/js/pop_up.js
+++ ALLbum_official/js/pop_up.js
@@ -1,23 +1,23 @@
 $(function() {
 
     var apiUrl = "http://allbum.in/allbum/api/1/official/album/face"
-    
+
     // face info GET
-    var getFaceAlbum = function(apiUrl, offset, callback) {
-        
+    var getFaceAlbum = function(apiUrl, type, offset, callback) {
+
         var faceAlbums = new Array();
-        
+
         $.ajax({
             type : "GET",
-            url : apiUrl + "?offset=" + offset,
+            url : apiUrl + "?dataType=" + type + "&offset=" + offset,
             success : function(res) {
 	            console.log(res);
 	            var data = res.response.data;
-	            
+
 	            $.each(data, function(index, row) {
 	                console.log(index);
 	            	console.log(row);
-	            	
+
 	            	var faceAlbum = new FaceAlbum();
 	            	faceAlbum.id = row.id;
 	            	faceAlbum.name = row.name;
@@ -32,11 +32,11 @@ $(function() {
            }
         });
     };
-    
-        
-    var setAlbum = function(faceAlbums){
+
+
+    var setAlbumPopular = function(faceAlbums){
         console.log(faceAlbums);
-        
+
         var $populars = $("#populars");
         var $ul = $("<ul>").addClass("thumbnails");
 
@@ -45,12 +45,29 @@ $(function() {
             var $thumbnail = $("<div>").addClass("thumbnail");
             var $img = $("<img>").attr("src", album.coverUrl).attr("title", album.name);
             var $caption = $("<p>").addClass("caption").text(album.name);
-            
+
             $populars.append($ul.append($li.append($thumbnail.append($img).append($caption))));
             //$populars.append($ul.append($li.append($img).append($caption)));
         });
     };
-    
+
+    var setAlbumRecent = function(faceAlbums){
+        console.log(faceAlbums);
+
+        var $populars = $("#recents");
+        var $ul = $("<ul>").addClass("thumbnails");
+
+        $.each(faceAlbums, function(index, album) {
+            var $li = $("<li>").addClass("span2").addClass("thumbnailRow");
+            var $thumbnail = $("<div>").addClass("thumbnail");
+            var $img = $("<img>").attr("src", album.coverUrl).attr("title", album.name);
+            var $caption = $("<p>").addClass("caption").text(album.name);
+
+            $populars.append($ul.append($li.append($thumbnail.append($img).append($caption))));
+            //$populars.append($ul.append($li.append($img).append($caption)));
+        });
+    };
+
     //constructor
     var FaceAlbum = function() {
     	this.id;
@@ -59,10 +76,11 @@ $(function() {
     	this.photos;
     	this.privacy;
     }
-    
+
     //execute
     for (i = 0; i < 5; i++){
-        getFaceAlbum(apiUrl, i, setAlbum);
+        getFaceAlbum(apiUrl, "popular", i, setAlbumPopular);
+        getFaceAlbum(apiUrl, "recent", i, setAlbumRecent);
     }
 
 
