65c0ba81b06d8ef49a05bfcbd1bb73a10fc8d999
yamashita_takeshi <yamashita_takeshi@172.17.90.139>
2013-03-06 02:16:41 +0900

    create

---
+++ social_counter/js/popup.js
@@ -0,0 +1,10 @@
+var social_counter_popup;
+(function (social_counter_popup) {
+    var showCounter = (function () {
+        function showCounter() {
+        }
+        showCounter.prototype.show = function () {
+        };
+        return showCounter;
+    })();    
+})(social_counter_popup || (social_counter_popup = {}));
---
+++ social_counter/js/popup.ts
@@ -0,0 +1,12 @@
+module social_counter_popup {
+	
+	class showCounter {
+		constructor() {
+
+		}
+
+		private show() {
+			
+		}
+	}
+}
