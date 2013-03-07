efe69bbcc3d16fc87dde3a3b1d4ad687861eb255
takecy <yamacy.takecy@gmail.com>
2013-03-07 15:55:41 +0900

    ts

--- social_counter/js/popup.ts
+++ social_counter/js/popup.ts
@@ -5,6 +5,21 @@ module Service {
 	export function getBgPage() {
 		return chrome.extension.getBackgroundPage();
 	}
+	export function sendRequest(request: string, callback) {
+		chrome.extension.sendRequest({id: request}, callback);
+	}
+	function receiveRequest() {
+		chrome.extension.onRequest.addListener(function(request, sender, callback) {
+			var id = request.id;
+			if(id === "getCount") {
+
+			} else if (id === "refresh") {
+
+			} else if (id === "delCache") {
+
+			}
+		});
+	}
 }
 
 module Processor {
