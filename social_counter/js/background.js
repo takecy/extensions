bd4c2198d4a5a7015ad4bb78782621e5cc394587
takecy <yamacy.takecy@gmail.com>
2013-03-08 13:13:03 +0900

    update

--- social_counter/js/background.js
+++ social_counter/js/background.js
@@ -1,101 +1,92 @@
+var __extends = this.__extends || function (d, b) {
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
 var ProviderService;
 (function (ProviderService) {
-    var HatenaService = (function () {
-        function HatenaService() {
-            this.apiUrl = 'http://b.hatena.ne.jp/entry/jsonlite/';
-        }
-        HatenaService.prototype.getCount = function (targetUrl) {
-            var apiJson = null;
+    var BaseProviderService = (function () {
+        function BaseProviderService() { }
+        BaseProviderService.prototype.getCount = function (targetUrl) {
+            var that = this;
+            var deferred = $.Deferred();
             fetchApi(this.apiUrl, targetUrl).done(function (json) {
-                console.log('[hatena]' + json);
-                apiJson = json;
+                console.log('[provider]');
+                console.log(json);
+                var count = that.parseJson(json);
+                console.log(count);
+                if(!count) {
+                    deferred.resolve(0);
+                } else {
+                    deferred.resolve(count);
+                }
             }).fail(function () {
-                console.log('[hatena]' + 'fail');
+                deferred.reject();
             });
-            if(apiJson) {
-                return apiJson['count'];
-            }
-            return 0;
+            return deferred.promise();
         };
+        return BaseProviderService;
+    })();
+    ProviderService.BaseProviderService = BaseProviderService;    
+    var HatenaService = (function (_super) {
+        __extends(HatenaService, _super);
+        function HatenaService() {
+            _super.apply(this, arguments);
+
+            this.apiUrl = 'http://b.hatena.ne.jp/entry/jsonlite/?url=';
+        }
         HatenaService.prototype.parseJson = function (json) {
             if(json) {
                 return json['count'];
             }
             return 0;
         };
-        HatenaService.prototype.render = function (count) {
-            return;
-        };
         return HatenaService;
-    })();
+    })(BaseProviderService);
     ProviderService.HatenaService = HatenaService;    
-    var TwitterService = (function () {
+    var TwitterService = (function (_super) {
+        __extends(TwitterService, _super);
         function TwitterService() {
+            _super.apply(this, arguments);
+
             this.apiUrl = 'http://urls.api.twitter.com/1/urls/count.json?url=';
         }
-        TwitterService.prototype.getCount = function (targetUrl) {
-            var apiJson = null;
-            fetchApi(this.apiUrl, targetUrl).done(function (json) {
-                console.log('[twitter]' + json);
-                apiJson = json;
-            }).fail(function () {
-                console.log('[twitter]' + 'fail');
-            });
-            if(apiJson) {
-                return apiJson['count'];
+        TwitterService.prototype.parseJson = function (json) {
+            if(json) {
+                return json['count'];
             }
             return 0;
-        };
-        TwitterService.prototype.parseJson = function (json) {
-            return json['count'];
-        };
-        TwitterService.prototype.render = function (count) {
-            return;
         };
         return TwitterService;
-    })();
+    })(BaseProviderService);
     ProviderService.TwitterService = TwitterService;    
-    var FacebookService = (function () {
+    var FacebookService = (function (_super) {
+        __extends(FacebookService, _super);
         function FacebookService() {
+            _super.apply(this, arguments);
+
             this.apiUrl = 'https://graph.facebook.com/';
         }
-        FacebookService.prototype.getCount = function (targetUrl) {
-            var apiJson = null;
-            fetchApi(this.apiUrl, targetUrl).done(function (json) {
-                console.log('[facebook]' + json);
-                apiJson = json;
-            }).fail(function () {
-                console.log('[facebook]' + 'fail');
-            });
-            if(apiJson) {
-                return apiJson['shares'];
+        FacebookService.prototype.parseJson = function (json) {
+            if(json) {
+                return json['shares'];
             }
             return 0;
         };
-        FacebookService.prototype.parseJson = function (json) {
-            var count = json['shares'];
-            if(!count) {
-                return 0;
-            }
-            return count;
-        };
-        FacebookService.prototype.render = function (count) {
-            return;
-        };
         return FacebookService;
-    })();
+    })(BaseProviderService);
     ProviderService.FacebookService = FacebookService;    
     function fetchApi(apiUrl, targetUrl) {
-        console.log('[fetchApi]' + apiUrl);
-        console.log('[fetchApi]' + targetUrl);
+        console.log('[fetchApi]' + apiUrl + targetUrl);
         var deferred = $.Deferred();
         $.ajax({
             type: 'GET',
             url: apiUrl + targetUrl,
             dataType: 'json'
         }).done(function (json, dataType) {
-            console.log('[fetchApi]' + dataType);
-            console.log('[fetchApi]' + json);
+            console.log('[fetchApi]');
+            console.log(dataType);
+            console.log(json);
             deferred.resolve(json, dataType);
         }).fail(function () {
             console.log('[fetchApi]' + 'fail');
@@ -124,10 +115,23 @@ var TabService;
         });
     }
     TabService.setBadge = setBadge;
-    function getCurrentTabUrl() {
+    function getColorCode(count) {
+        if(count < 10) {
+            return '#7aacf3';
+        } else if(count >= 10) {
+            return '#5c91dd';
+        } else if(count >= 100) {
+            return '#3e72bd';
+        } else if(count >= 300) {
+            return '#285f97';
+        }
+    }
+    TabService.getColorCode = getColorCode;
+    function getCurrentTab() {
         var deferred = $.Deferred();
         chrome.tabs.getCurrent(function (tab) {
-            console.log('[tab]' + tab);
+            console.log('[current tab]');
+            console.log(tab);
             if(!tab) {
                 deferred.reject();
             } else {
@@ -136,7 +140,21 @@ var TabService;
         });
         return deferred.promise();
     }
-    TabService.getCurrentTabUrl = getCurrentTabUrl;
+    TabService.getCurrentTab = getCurrentTab;
+    function getTabInfo(tabId) {
+        var deferred = $.Deferred();
+        chrome.tabs.get(tabId, function (tab) {
+            console.log('[tab]');
+            console.log(tab);
+            if(!tab) {
+                deferred.reject();
+            } else {
+                deferred.resolve(tab.url);
+            }
+        });
+        return deferred.promise();
+    }
+    TabService.getTabInfo = getTabInfo;
     function openTab(url, callback) {
         chrome.tabs.create({
             url: url
@@ -162,22 +180,78 @@ var Message;
         });
     }
 })(Message || (Message = {}));
+var Animation;
+(function (Animation) {
+    function rotateIcon() {
+        var iconPath = 'img/google_128.png';
+        var canvas = $("<canvas>").attr("width", "19").attr("height", "19")[0];
+        var icon = new Image();
+        icon.src = iconPath;
+        var ctx = canvas.getContext('2d');
+        var canvasWidth = canvas.width;
+        var canvasHeight = canvas.height;
+        var cx = canvasWidth / 2;
+        var cy = canvasHeight / 2;
+        ctx.translate(cx, cy);
+        var count = 0;
+        var rotate = function () {
+            var rotationAngle = 10;
+            var rotationAllAngle = 360 * 2;
+            ctx.clearRect(-cx, -cy, canvasWidth, canvasHeight);
+            ctx.rotate(rotationAngle * Math.PI / 180);
+            ctx.drawImage(icon, -cx, -cy, canvasWidth, canvasHeight);
+            chrome.browserAction.setIcon({
+                imageData: ctx.getImageData(0, 0, canvasWidth, canvasHeight)
+            });
+            if(++count >= (rotationAllAngle / rotationAngle)) {
+                clearInterval(timer);
+            }
+        };
+        var timer = setInterval(rotate, 1);
+    }
+    Animation.rotateIcon = rotateIcon;
+    ;
+})(Animation || (Animation = {}));
 var Processor;
 (function (Processor) {
-    var badge = new TabService.Badge('9999', '#FF0000');
+    var badge = new TabService.Badge('-', TabService.getColorCode(0).toString());
     TabService.setBadge(badge);
-    var targetUrl = 'http://google.co.jp';
-    TabService.getCurrentTabUrl().done(function (url) {
-        console.log('[tab]' + url);
-        targetUrl = url;
-    }).fail(function () {
-        console.log('[tab]' + 'fail');
-    });
-    (function () {
+    var getAllCount = function (targetUrl) {
         var hatena = new ProviderService.HatenaService();
         var facebook = new ProviderService.FacebookService();
         var twitter = new ProviderService.TwitterService();
-        var hatenaCount = hatena.getCount(targetUrl);
-        console.log('[hatenaCount]' + hatenaCount);
-    })();
+        $.when(hatena.getCount(targetUrl), facebook.getCount(targetUrl), twitter.getCount(targetUrl)).done(function (hatenaCount, likeCount, tweetCount) {
+            console.log('[hatena]' + hatenaCount);
+            console.log('[like]' + likeCount);
+            console.log('[tweet]' + tweetCount);
+            var totalCount = parseInt(hatenaCount) + parseInt(likeCount) + parseInt(tweetCount);
+            console.log('[total]' + totalCount);
+            var colorCode = TabService.getColorCode(totalCount).toString();
+            Animation.rotateIcon();
+            var badge = new TabService.Badge(totalCount.toString(), colorCode);
+            TabService.setBadge(badge);
+        }).fail(function () {
+            console.log('fail');
+        });
+    };
+    chrome.tabs.onCreated.addListener(function (tab) {
+        var tabId = tab.tabId;
+        var tabUrl = tab.url;
+        console.log('[onCreated]' + tabUrl);
+        getAllCount(tabUrl);
+    });
+    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+        var tabUrl = tab.url;
+        console.log('[onUpdated]' + tabUrl);
+        getAllCount(tabUrl);
+    });
+    chrome.tabs.onActivated.addListener(function (activeInfo) {
+        var tabId = activeInfo.tabId;
+        console.log('[onActivated]' + tabId);
+        TabService.getTabInfo(tabId).done(function (tabUrl) {
+            getAllCount(tabUrl);
+        }).fail(function () {
+            console.log('[onActivated]' + 'fail');
+        });
+    });
 })(Processor || (Processor = {}));
--- social_counter/js/background.ts
+++ social_counter/js/background.ts
@@ -4,95 +4,63 @@ module ProviderService {
 
 	export interface IProviderService {
 		apiUrl: string;
-		getCount: (targetUrl: string) => number;
+		getCount: (targetUrl: string) => any;
 		parseJson: (json: string) => number;
-		render: (count: number) => void;
 	}
 
-	export class HatenaService implements IProviderService {
-		private apiUrl = 'http://b.hatena.ne.jp/entry/jsonlite/';
-		
-		getCount(targetUrl: string): number {
-			var apiJson = null;
+	export class BaseProviderService implements IProviderService {
+		getCount(targetUrl: string): any {
+			var that = this;
+			var deferred = $.Deferred();
+
 			fetchApi(this.apiUrl, targetUrl)
 			.done(function(json){
-				console.log('[hatena]' + json);
-				apiJson = json;
+				console.log('[provider]');
+				console.log(json);
+
+				var count = that.parseJson(json);
+				console.log(count);
+
+				if(!count) deferred.resolve(0);
+				else deferred.resolve(count);
 			})
 			.fail(function(){
-				console.log('[hatena]' + 'fail');
+				deferred.reject();
 			});
 
-            if(apiJson) return apiJson['count'];
-            return 0;
+			return deferred.promise();
 		}
+	}
+
+	export class HatenaService extends BaseProviderService {
+		private apiUrl = 'http://b.hatena.ne.jp/entry/jsonlite/?url=';
+		
 		parseJson(json: string): number {
             if(json) return json['count'];
             return 0;
 		}
-		render(count: number): void {
-			//TODO
-			return;
-		}
 	}
 
-	export class TwitterService implements IProviderService {
+	export class TwitterService extends BaseProviderService {
 		private apiUrl = 'http://urls.api.twitter.com/1/urls/count.json?url=';
 
-		getCount(targetUrl: string): number {
-			var apiJson = null;
-			fetchApi(this.apiUrl, targetUrl)
-			.done(function(json){
-				console.log('[twitter]' + json);
-				apiJson = json;
-			})
-			.fail(function(){
-				console.log('[twitter]' + 'fail');
-			});
-
-            if(apiJson) return apiJson['count'];
-            return 0;
-		}
 		parseJson(json: string): number {
-            return json['count'];
-		}
-		render(count: number): void {
-			//TODO
-			return;
+            if(json) return json['count'];
+            return 0;
 		}
 	}
 
-	export class FacebookService implements IProviderService {
+	export class FacebookService extends BaseProviderService {
 		private apiUrl = 'https://graph.facebook.com/';
 
-		getCount(targetUrl: string): number {
-			var apiJson = null;
-			fetchApi(this.apiUrl, targetUrl)
-			.done(function(json){
-				console.log('[facebook]' + json);
-				apiJson = json;
-			})
-			.fail(function(){
-				console.log('[facebook]' + 'fail');
-			});
-
-            if(apiJson) return apiJson['shares'];
-            return 0;
-		}
 		parseJson(json: string): number {
-	     	var count = json['shares'];
-	        if(!count) return 0;
-	        return count;
-		}
-		render(count: number): void {
-			//TODO
-			return;
+            if(json) return json['shares'];
+            return 0;
 		}
 	}
 
 	function fetchApi(apiUrl: string, targetUrl: string) {	
-    	console.log('[fetchApi]' + apiUrl);
-    	console.log('[fetchApi]' + targetUrl);
+    	console.log('[fetchApi]' + apiUrl + targetUrl);
 
 		var deferred = $.Deferred();
 		$.ajax({
@@ -101,11 +69,12 @@ module ProviderService {
             dataType : 'json'
         })
         .done(function(json, dataType) {
-        	console.log('[fetchApi]' + dataType);
-        	console.log('[fetchApi]' + json);
+        	console.log('[fetchApi]');
+        	console.log(dataType);
+        	console.log(json);
         	deferred.resolve(json, dataType);
     	})
-    	.fail(function() {
+    	.fail(function(){
         	console.log('[fetchApi]' + 'fail');
     		deferred.reject();	
 		});
@@ -123,13 +92,19 @@ module TabService {
 		chrome.browserAction.setBadgeBackgroundColor({color: badge.color});
 	}
 
-	//TODO when tab selected and updated, add listner event.
-	export function getCurrentTabUrl() {
+	export function getColorCode(count: number): string{
+	    if(count < 10) return '#7aacf3';
+	    else if(count >= 10) return '#5c91dd';
+	    else if(count >= 100) return '#3e72bd';
+	    else if(count >= 300) return '#285f97';
+	}
+
+	export function getCurrentTab(): any {
 		var deferred = $.Deferred();
 
 		chrome.tabs.getCurrent(function(tab){
-			console.log('[tab]' + tab);
-			//取得不許可のページの場合
+			console.log('[current tab]');
+			console.log(tab);
 			if(!tab) {
 				deferred.reject();
 			} else {
@@ -139,6 +114,23 @@ module TabService {
 			
 		return deferred.promise();
 	}
+
+	export function getTabInfo(tabId: number): any {
+		var deferred = $.Deferred();
+
+		chrome.tabs.get(tabId, function(tab){
+			console.log('[tab]');
+			console.log(tab);
+			if(!tab) {
+				deferred.reject();
+			} else {
+				deferred.resolve(tab.url);
+			}
+		});
+
+		return deferred.promise();
+	}
+
 	export function openTab(url: string, callback: any) {
 		chrome.tabs.create({url: url}, callback);
 	}
@@ -162,34 +154,104 @@ module Message {
 	}
 }
 
+module Animation {
+	export function rotateIcon() {
+		var iconPath = 'img/google_128.png';
+		var canvas = $("<canvas>").attr("width","19").attr("height","19")[0];
+		var icon = new Image();
+		icon.src = iconPath;
+		
+		//コンテキストの取得
+		var ctx = canvas.getContext('2d');
+		//canvasの高さと幅
+		var canvasWidth = canvas.width;
+		var canvasHeight = canvas.height;
+		//中心座標
+		var cx = canvasWidth / 2;
+		var cy = canvasHeight / 2;
+		
+		//中心座標を移動
+		ctx.translate(cx, cy);
+		var count = 0;
+		//1回の回転処理
+		var rotate = function() {
+			//1回の回転角度
+			var rotationAngle = 10;
+			//総回転角度
+			var rotationAllAngle = 360*2;
+			//canvas消去
+			ctx.clearRect(-cx, -cy, canvasWidth, canvasHeight);
+			//回転実行
+			ctx.rotate(rotationAngle * Math.PI / 180);
+			//描画
+			ctx.drawImage(icon, -cx, -cy, canvasWidth, canvasHeight);
+			//アイコン設定
+			chrome.browserAction.setIcon({imageData : ctx.getImageData(0, 0, canvasWidth, canvasHeight)});
+			//回転終わったら実行解除
+			if(++count >= (rotationAllAngle / rotationAngle)) clearInterval(timer);
+		};
+		//定期実行
+		var timer = setInterval(rotate, 1);
+	};
+
+}
+
 module Processor {
-	var badge = new TabService.Badge('9999', '#FF0000');
+
+	var badge = new TabService.Badge('-', TabService.getColorCode(0).toString());
 	TabService.setBadge(badge);
 
-	var targetUrl = 'http://google.co.jp';
-	TabService.getCurrentTabUrl()
-	.done(function(url){
-		console.log('[tab]' + url);
-		targetUrl = url;
-	})
-	.fail(function(){
-		console.log('[tab]' + 'fail');
-	});
-
-
-	(function() {
+	var getAllCount = function(targetUrl: string) {
 		var hatena = new ProviderService.HatenaService();
 		var facebook = new ProviderService.FacebookService();
 		var twitter = new ProviderService.TwitterService();
 
-		var hatenaCount = hatena.getCount(targetUrl);
-		console.log('[hatenaCount]' + hatenaCount);
+		$.when(hatena.getCount(targetUrl), facebook.getCount(targetUrl), twitter.getCount(targetUrl))
+		.done(function(hatenaCount, likeCount, tweetCount){
+			console.log('[hatena]' + hatenaCount);
+			console.log('[like]' + likeCount);
+			console.log('[tweet]' + tweetCount);
 
-//		var likeCount = facebook.getCount(targetUrl);
-//		console.log('[likeCount]' + likeCount);
+			var totalCount = parseInt(hatenaCount) + parseInt(likeCount) + parseInt(tweetCount);
+			console.log('[total]' + totalCount);
+			var colorCode = TabService.getColorCode(totalCount).toString();
 
-//		var tweetCount = twitter.getCount(targetUrl);
-//		console.log('[tweetCount]' + tweetCount);
-	})();
+			Animation.rotateIcon();
+			var badge = new TabService.Badge(totalCount.toString(), colorCode);
+			TabService.setBadge(badge);
+		})
+		.fail(function(){
+			console.log('fail');
+		});
+	};
+
+	//create
+	chrome.tabs.onCreated.addListener(function(tab) {
+		var tabId = tab.tabId;
+		var tabUrl = tab.url;
+		console.log('[onCreated]' + tabUrl);
+		getAllCount(tabUrl);
+	});
+
+	//update
+	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+		var tabUrl = tab.url;
+		console.log('[onUpdated]' + tabUrl);
+		getAllCount(tabUrl);
+	});
+
+	//activate
+	chrome.tabs.onActivated.addListener(function(activeInfo) {
+		var tabId = activeInfo.tabId;
+		console.log('[onActivated]' + tabId);
+
+		TabService.getTabInfo(tabId)
+		.done(function(tabUrl){
+			getAllCount(tabUrl);
+		})
+		.fail(function(){
+			console.log('[onActivated]' + 'fail');
+		});
+	});
 
 }
