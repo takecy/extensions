02716a8d7218ddab6809d3780d221b382444af6f
takeshi yamashita <yamacy.takecy@gmail.com>
2013-03-11 01:42:46 +0900

    bug fix

--- all_tab_pined/js/background.js
+++ all_tab_pined/js/background.js
@@ -1,5 +1,5 @@
 chrome.commands.onCommand.addListener(function(command) {
-  if (command == "toggle-pin") {
+  if (command == "toggle-pin-all") {
     // Get the currently selected tab
     chrome.windows.getCurrent({populate: true}, function(window_) {
       // Toggle the pinned status
@@ -10,6 +10,11 @@ chrome.commands.onCommand.addListener(function(command) {
       	var tab = tabs[i];
       	chrome.tabs.update(tab.id, {'pinned': !tab.pinned});
       }
+    });
+  }
+  else if(command == "toggle-pin") {
+    chrome.tabs.getSelected(function(tab){
+        chrome.tabs.update(tab.id, {'pinned': !tab.pinned});
     });
   }
 });
