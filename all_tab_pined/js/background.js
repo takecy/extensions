8458130db29b591de385d26c45ee77f4d9dbbee2
takeshi yamashita <yamacy.takecy@gmail.com>
2013-03-11 00:19:52 +0900

    bug fix

--- all_tab_pined/js/background.js
+++ all_tab_pined/js/background.js
@@ -1,12 +1,14 @@
 chrome.commands.onCommand.addListener(function(command) {
   if (command == "toggle-pin") {
     // Get the currently selected tab
-    chrome.windows.getCurrent(null, function(window_) {
+    chrome.windows.getCurrent({populate: true}, function(window_) {
       // Toggle the pinned status
       var tabs = window_.tabs;
-      for(int=i; i<tabs.length; i++) {
+      console.log(tabs);
+      tabs.reverse();
+      for(i=0; i<tabs.length; i++) {
       	var tab = tabs[i];
-      	chrome.tab.update(tab.id, {'pinned': !tab.pinned});
+      	chrome.tabs.update(tab.id, {'pinned': !tab.pinned});
       }
     });
   }
